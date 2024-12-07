771fb3a68888129cf44baf207d75335ab07cebc3
Dheeren <ganeshgaud1111@gmail.com>
2024-12-07 18:01:38 +0530

    added fronted

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import OlaMapsComponent from "./components/OlaMapsComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ola Maps Integration</h1>
+      <OlaMapsComponent />
+    </div>
+  );
+}
+
+export default App;
+
